092d6767ce77fc302df037b172dcf1a66c2b3882
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-09-14 14:10:38 -0500

    feat: add MessageEvent class for encapsulating key-value pairs in Kafka events

---
+++ practice/src/main/java/com/practice/kafka/event/MessageEvent.java
@@ -0,0 +1,12 @@
+package com.practice.kafka.event;
+
+public class MessageEvent {
+    String key;
+    String value;
+
+    public MessageEvent(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+}
